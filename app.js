f9ccabd5ada73540559f63b61c97f80074d893e4
AvazAlimov <avazalimov95@mail.ru>
2019-03-20 14:17:36 +0500

    Cors changed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // app.use(static(join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 indexRouter(app);
