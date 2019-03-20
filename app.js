32597f7d0750af3616034990bd738dbd47e37b48
AvazAlimov <avazalimov95@mail.ru>
2019-03-20 13:49:10 +0500

    Cors bug fixed

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // app.use(static(join(__dirname, 'public')));
 
 indexRouter(app);
