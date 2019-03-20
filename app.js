28b50999992837a54ce6704762f02e174e01aa16
AvazAlimov <avazalimov95@mail.ru>
2019-03-20 14:32:55 +0500

    CORS module readded

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
-// import cors from 'cors';
+import cors from 'cors';
 import indexRouter from './routes/index';
 
 dotenv.config();
@@ -20,11 +20,7 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(static(join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
+app.use(cors({ origin: '*' }));
 indexRouter(app);
 
 // catch 404 and forward to error handler
