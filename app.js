c6bcef5c54f7a8f34b4cbda85b909778ddba18bd
AvazAlimov <avazalimov95@mail.ru>
2019-03-22 14:13:47 +0500

    Static files references updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(static(join(__dirname + '../Texnaz_client/', 'public')));
+app.use(express.static(join(__dirname, '../Texnaz_client/dist')));
 app.use(cors());
 indexRouter(app);
 
