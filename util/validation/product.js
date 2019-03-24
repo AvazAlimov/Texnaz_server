504c68e505db152c6ff4106461bdef0f185e65ea
AvazAlimov <avazalimov95@mail.ru>
2019-03-24 18:26:32 +0500

    Product validation modified

--- util/validation/product.js
+++ util/validation/product.js
@@ -1,56 +1,9 @@
 import { validationResult, checkSchema } from 'express-validator/check';
 import models from '../../models';
-import checkRules from './rule';
 
-function getRuleFields(req) {
-  return [{
-    id: req.body.unit,
-    model: models.Unit,
-    name: 'Unit',
-  },
-  {
-    id: req.body.type,
-    model: models.Type,
-    name: 'Type',
-  },
-  {
-    id: req.body.purpose,
-    model: models.Purpose,
-    name: 'Purpose',
-  },
-  {
-    id: req.body.tag,
-    model: models.Tag,
-    name: 'Tag',
-  },
-  ];
-}
-
-export function validate(req, res, next) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(403).json({
-      errors: errors.array(),
-    });
-  } else {
-    checkRules(getRuleFields(req), res, () => {
-      req.product = {
-        name: req.body.name,
-        packing: req.body.packing,
-        color: req.body.color,
-        unit: req.body.unit,
-        type: req.body.type,
-        cleaning: req.body.cleaning,
-        vat: req.body.vat,
-        tax: req.body.tax,
-        excise: req.body.excise,
-        code: req.body.ratio || null,
-        purpose: req.body.purpose || null,
-        tag: req.body.tag || null,
-      };
-      next();
-    });
-  }
+async function exists(model, value) {
+  const units = (await model.findAll({ attributes: ['id'], raw: true })).map(id => id.id);
+  return units.includes(value);
 }
 
 export const check = checkSchema({
@@ -62,12 +15,6 @@ export const check = checkSchema({
   },
   color: {
     isString: true,
-  },
-  unit: {
-    isInt: true,
-  },
-  type: {
-    isInt: true,
   },
   cleaning: {
     isFloat: true,
@@ -83,14 +30,56 @@ export const check = checkSchema({
   },
   code: {
     optional: true,
-    isString: true,
+  },
+  unit: {
+    isInt: true,
+    custom: {
+      options: value => exists(models.Unit, value),
+    },
+  },
+  type: {
+    isInt: true,
+    custom: {
+      options: value => exists(models.Type, value),
+    },
   },
   purpose: {
     optional: true,
     isInt: true,
+    custom: {
+      options: value => exists(models.Purpose, value),
+    },
   },
   tag: {
     optional: true,
     isInt: true,
+    custom: {
+      options: value => exists(models.Tag, value),
+    },
   },
 });
+
+export function validate(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(403).json({
+      errors: errors.array(),
+    });
+  } else {
+    req.product = {
+      name: req.body.name,
+      packing: req.body.packing,
+      color: req.body.color,
+      unit: req.body.unit,
+      type: req.body.type,
+      cleaning: req.body.cleaning,
+      vat: req.body.vat,
+      tax: req.body.tax,
+      excise: req.body.excise,
+      code: req.body.ratio || null,
+      purpose: req.body.purpose || null,
+      tag: req.body.tag || null,
+    };
+    next();
+  }
+}
