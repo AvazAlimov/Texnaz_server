50af4f821fc3ee9b67ec60a3acea6e939b1deceb
AvazAlimov <avazalimov95@mail.ru>
2019-04-25 12:25:17 +0500

    Cors modified

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: '*',
+  origin: false,
   allowedHeaders: 'Content-Type, Authorization',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
