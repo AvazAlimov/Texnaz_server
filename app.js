2dcf7dcf756fcce9257ae6434a466b2c587e2d5e
AvazAlimov <avazalimov95@mail.ru>
2019-04-25 10:18:08 +0500

    Cors modified

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ dotenv.config();
 
 const app = express();
 app.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   next();
 });
 
