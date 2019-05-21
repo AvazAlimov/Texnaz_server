5a4675c92161912cf73e2764b48a52807c4e49c4
AvazAlimov <avazalimov95@mail.ru>
2019-05-21 19:17:04 +0500

    Managers seeder added

--- seeders/20190311072626-users.js
+++ seeders/20190311072626-users.js
@@ -25,6 +25,61 @@ module.exports = {
     username: 'seller',
     password: bcrypt.hashSync('seller', 10),
   },
+
+  {
+    id: 5,
+    name: 'Руслан Нусеров',
+    username: 'nuserov',
+    password: bcrypt.hashSync('nuserov', 10),
+  },
+  {
+    id: 6,
+    name: 'Курбан',
+    username: 'kurban',
+    password: bcrypt.hashSync('kurban', 10),
+  },
+  {
+    id: 7,
+    name: 'Даврон',
+    username: 'davron',
+    password: bcrypt.hashSync('davron', 10),
+  },
+  {
+    id: 8,
+    name: 'Рафик',
+    username: 'rafik',
+    password: bcrypt.hashSync('rafik', 10),
+  },
+  {
+    id: 9,
+    name: 'Дониер',
+    username: 'doniyor',
+    password: bcrypt.hashSync('doniyor', 10),
+  },
+  {
+    id: 10,
+    name: 'Санжар',
+    username: 'sanjar',
+    password: bcrypt.hashSync('sanjar', 10),
+  },
+  {
+    id: 11,
+    name: 'Рустам',
+    username: 'rustam',
+    password: bcrypt.hashSync('rustam', 10),
+  },
+  {
+    id: 12,
+    name: 'Ахматхужа',
+    username: 'ahmatxuja',
+    password: bcrypt.hashSync('ahmatxuja', 10),
+  },
+  {
+    id: 13,
+    name: 'Наиля',
+    username: 'nailya',
+    password: bcrypt.hashSync('nailya', 10),
+  },
   ], {}),
 
   down: queryInterface => queryInterface.bulkDelete('Users', null, {}),
--- seeders/20190311082048-user-role.js
+++ seeders/20190311082048-user-role.js
@@ -1,21 +1,30 @@
+function bindManagers(managers) {
+  return managers.map(userId => ({ userId, roleId: 2 }));
+}
+
 module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('UserRoles', [{
-    userId: 1,
-    roleId: 1,
-  },
-  {
-    userId: 2,
-    roleId: 2,
-  },
-  {
-    userId: 3,
-    roleId: 3,
-  },
-  {
-    userId: 4,
-    roleId: 4,
-  },
-  ], {}),
+  up: queryInterface => queryInterface.bulkInsert(
+    'UserRoles',
+    [
+      {
+        userId: 1,
+        roleId: 1,
+      },
+      {
+        userId: 2,
+        roleId: 2,
+      },
+      {
+        userId: 3,
+        roleId: 3,
+      },
+      {
+        userId: 4,
+        roleId: 4,
+      },
+    ].concat(bindManagers([5, 6, 7, 8, 9, 10, 11, 12, 13])),
+    {},
+  ),
 
   down: queryInterface => queryInterface.bulkDelete('UserRoles', null, {}),
 };
