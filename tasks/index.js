0588c2c42576a25c2ff64913ca8e12fe16a4364a
AvazAlimov <avazalimov95@mail.ru>
2019-06-25 21:46:58 +0500

    Command modified

--- tasks/index.js
+++ tasks/index.js
@@ -15,14 +15,12 @@ const job = new CronJob('0 0 */1 * * *', () => {
 
 const backup = new CronJob(process.env.BACKUP_DELAY || '0 0 */1 * * *', () => {
   const user = process.env.DB_USERNAME;
-  const password = process.env.DB_DB_PASSWORD || '';
+  const password = process.env.DB_PASSWORD || '';
   const database = process.env.DB_DATABASE;
   const filename = (new Date()).toISOString().substring(0, 16).replace(':', '');
   const folder = process.env.BACKUP_FOLDER || 'backup';
   const command = `cd ${folder} && mysqldump -u ${user} -p${password} ${database} > ${filename}.sql`;
-  console.log(command);
   exec(command);
-  console.log((new Date()).toLocaleTimeString());
 });
 
 job.start();
