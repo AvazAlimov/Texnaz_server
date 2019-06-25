6aa6b4878f1b26c553438d33fbc69852dcaf7345
AvazAlimov <avazalimov95@mail.ru>
2019-06-25 21:28:05 +0500

    Backup added

--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,6 @@
 import { CronJob } from 'cron';
 import moment from 'moment';
+import { exec } from 'child_process';
 import models from '../models';
 
 const job = new CronJob('0 0 */1 * * *', () => {
@@ -12,4 +13,16 @@ const job = new CronJob('0 0 */1 * * *', () => {
   });
 });
 
+const backup = new CronJob(process.env.BACKUP_DELAY || '0 0 */1 * * *', () => {
+  const user = process.env.DB_USERNAME;
+  const password = process.env.DB_DB_PASSWORD || '';
+  const database = process.env.DB_DATABASE;
+  const filename = (new Date()).toISOString().substring(0, 16).replace(':', '');
+  const folder = process.env.BACKUP_FOLDER || 'backup';
+  const command = `cd ${folder} && mysqldump -u ${user} -p ${password} ${database} > ${filename}.sql`;
+  exec(command);
+  console.log((new Date()).toLocaleTimeString());
+});
+
 job.start();
+backup.start();
