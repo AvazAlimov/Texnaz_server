2b504b3458a1140ecbb6fcbe6b7bea3be2417658
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-09 02:44:09 +0500

    New client sales added

---
+++ seeders/20190907142540-sales.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  up: queryInterface => queryInterface.bulkInsert('Sales', [
+    {
+      number: '123',
+      isClosed: false,
+      type: 1,
+      form: 1,
+      clientId: 1,
+      managerId: 1,
+      userId: null,
+      warehouseId: 1,
+      approved: 0,
+      shipped: 0,
+      days: 1,
+      provinceId: 1,
+      officialRate: '1',
+      exchangeRate: '1',
+      createdAt: new Date(),
+    },
+  ], {}),
+
+  down: queryInterface => queryInterface.bulkDelete('Sales', null, {}),
+};
