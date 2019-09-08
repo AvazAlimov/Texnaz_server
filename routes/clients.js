9f6ef6d716140721d4d859b540c764351d921937
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-09 02:42:55 +0500

    routes added to clients and sales

--- routes/clients.js
+++ routes/clients.js
@@ -9,5 +9,5 @@ router.get('/:id', authMiddleware, client.get);
 router.post('/', authMiddleware, check, validate, client.create);
 router.post('/:id', authMiddleware, check, validate, client.update);
 router.delete('/:id', authMiddleware, client.delete);
-
+router.post('/balance/:id', authMiddleware, client.updateBalance);
 export default router;
--- routes/sales.js
+++ routes/sales.js
@@ -13,5 +13,7 @@ router.post('/approve/:id', authMiddleware, sale.approve);
 router.post('/disapprove/:id', authMiddleware, sale.disapprove);
 router.post('/shipment/approve/:id', authMiddleware, sale.approveShipment);
 router.post('/shipment/reject/:id', authMiddleware, sale.rejectShipment);
+router.post('/saleItem/:id', authMiddleware, sale.updateSaleItem);
+router.post('/close/:id', authMiddleware, sale.close);
 
 export default router;
