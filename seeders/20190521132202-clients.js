c20ab214e59f7b5dc9dfa80deb604558cddf5987
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-09 02:43:18 +0500

    new client added

--- seeders/20190521132202-clients.js
+++ seeders/20190521132202-clients.js
@@ -11,6 +11,12 @@ module.exports = {
     'Clients',
     [
       {
+        icc: 9559,
+        managerId: 21,
+        name: '"Steve" МЧЖ',
+        regionId: getRegionId('Андижанская область'),
+      },
+      {
         icc: 3001,
         managerId: 9,
         name: 'VOSTOK STROY DETAL" МЧЖ',
