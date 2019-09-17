0ea04ea51f80be3805b4e04ba28250fade00a11d
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-17 18:03:08 +0500

    Territory names

--- seeders/20190830124532-territories.js
+++ seeders/20190830124532-territories.js
@@ -4,19 +4,19 @@ module.exports = {
   up: queryInterface => queryInterface.bulkInsert('Territories', [
     {
       id: 1,
-      name: 'Territory 1',
+      name: 'Ташкент + Ташкент область',
     },
     {
       id: 2,
-      name: 'Territory 2',
+      name: 'Долина',
     },
     {
       id: 3,
-      name: 'Territory 3',
+      name: 'Югозапад 1',
     },
     {
       id: 4,
-      name: 'Territory 4',
+      name: 'Югозапад 2',
     },
   ], {}),
 
