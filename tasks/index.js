c52e3ad9168670d36eb64d156f47f455e03c5358
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-01-31 19:49:58 +0500

    setTimeOut helped for backup file

--- tasks/index.js
+++ tasks/index.js
@@ -23,7 +23,9 @@ const backup = new CronJob(process.env.BACKUP_DELAY || '0 0 */1 * * *', () => {
   const command = `cd ${folder} && mysqldump -u ${user} -p${password} ${database} > ${filename}.sql`;
   exec(command, (error) => {
     if (!error) {
-      sendBackup(`${folder}/${filename}.sql`, `${filename}.sql`);
+      setTimeout(() => {
+        sendBackup(`${folder}/${filename}.sql`, `${filename}.sql`);
+      }, 500);
     }
   });
 });
